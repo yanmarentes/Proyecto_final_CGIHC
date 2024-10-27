2058fa7557d8910f736b42a4737cedf9abbeac3a
Rodrigo Reyes <rodrigo.m.reyes.v@gmail.com>
2024-10-27 12:35:19 -0600

    add common values

--- Monopoly/Monopoly/CommonValues.h
+++ Monopoly/Monopoly/CommonValues.h
@@ -4,4 +4,22 @@
 
 const int MAX_POINT_LIGHTS = 2;
 const int MAX_SPOT_LIGHTS = 5;
+
+#define MAIN_DISTANCE_CORNER 67.5
+#define VEHICLES_DISTANCE_CORNER 100
+
+struct Ubicacion {
+	float x;
+	float y;
+	float z;
+};
+
+enum State_M_Mov {
+	STATE_REPOSO,
+	STATE_TIRAR_DADO,
+	STATE_TIRANDO_DADO,
+	STATE_EJECUTAR_TIRADA_DADO,
+	STATE_EJECUTANDO_TIRADA_DADO
+};
+
 #endif
